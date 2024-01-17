39850b788676c167dbb8aa3600f4f0abf20965b1
Kylie Clark <kyliedarlene@icloud.com>
2024-01-17 13:50:47 -0700

    TakList: deliverable complete, Task: complete except for delete functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={TASKS}/>
     </div>
   );
 }
