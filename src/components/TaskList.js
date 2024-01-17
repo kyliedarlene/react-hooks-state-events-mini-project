be1ba5017bf045d8baa08c7035a73626c8c07e92
Kylie Clark <kyliedarlene@icloud.com>
2024-01-17 14:53:54 -0700

    CategoryFilter: now filters based on selectedCategory state

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Task from "./Task";
 
-function TaskList({ 
-  tasks,
-  selectedCategory
-}) 
-{
+function TaskList({ tasks, selectedCategory }) {
   return (
     <div className="tasks">
-      {tasks.map((task) => (
-        <Task key={task.text} category={task.category} text={task.text} />
-      ))}
+      {tasks.map((task) => {
+        if ((selectedCategory === "All") || (selectedCategory === task.category)) {
+          return <Task key={task.text} category={task.category} text={task.text} />
+        }
+      })}
     </div>
   );
 }
